3bdffc5f74beb5f40a32b18195ee7917bb5015c6
Mubtasim-Prime <mubtasimfuadrafiq@gmail.com>
2024-07-14 00:22:06 +0600

    Created dynamic card components for projects page

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -4,13 +4,15 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import HeroImg2 from '../components/HeroImg2';
 import PricingCart from '../components/PricingCart';
+import Work from '../components/Work';
 
 const Project = () => {
   return (
     <div>
       <Navbar />
-      <HeroImg2 heading="PROJECTS." text="Some of my most recent works"/>
-      <PricingCart/>
+      <HeroImg2 heading="PROJECTS." text="Some of my most recent works" />
+      <Work />
+      <PricingCart />
       <Footer />
     </div>
   )
