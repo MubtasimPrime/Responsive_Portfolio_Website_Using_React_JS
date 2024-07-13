ea4002e27121776396bea6bf46e486bbd32d3e12
Mubtasim-Prime <mubtasimfuadrafiq@gmail.com>
2024-07-13 11:11:20 +0600

    Rendering components in different pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import "./FooterStyles.css";
 
 import React from 'react';
-import { FaHome } from "react-icons/fa";
+import { FaHome, FaPhone, FaMailBulk, FaFacebook, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 const footer = () => {
     return (
@@ -9,10 +9,34 @@ const footer = () => {
             <div className="footer-container">
                 <div className="left">
                     <div className="location">
-                        <FaHome />
+                        <FaHome size={20} style={{ color: "#fff", marginRight: "2rem" }} />
+
+                        <div>
+                            <p>123 Housing Society.</p>
+                            <p>Bangladesh</p>
+                        </div>
+                    </div>
+                    <div className="Phone">
+                        <h4><FaPhone size={20} style={{ color: "#fff", marginRight: "2rem" }} />
+                            1-2323-343-23</h4>
+                    </div>
+                    <div className="Email">
+                        <h4><FaMailBulk size={20} style={{ color: "#fff", marginRight: "2rem" }} />
+                            info@gmail.com</h4>
                     </div>
                 </div>
-                <div className="right"></div>
+
+                <div className="right">
+                    <h4>About the company</h4>
+                    <p>This is me Md. Mubtasim Fuad, CEO & Founder of GenTech. I enjoy discussing new projects and design challenges.</p>
+                    <div className="social">
+                        <FaFacebook size={30} style={{ color: "#fff", marginRight: "1rem" }} />
+
+                        <FaTwitter size={30} style={{ color: "#fff", marginRight: "1rem" }} />
+
+                        <FaLinkedin size={30} style={{ color: "#fff", marginRight: "1rem" }} />
+                    </div>
+                </div>
             </div>
         </div>
     )
